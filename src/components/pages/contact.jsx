86334c6dd6502b9ba85ac5b36b165781e2d68f26
Christina <christina.brgs@gmail.com>
2024-08-30 15:22:52 -0400

    updates

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -1,19 +1,29 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import '../../App.css'
 import emailjs from '@emailjs/browser'
 
 function Contact () {
+  const [buttonText, setButtonText] = useState("Send");
 
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_e9zb4ij', 'template_sfr49ca', form.current, 'V0TyvgfaL_nV9KGdZ')
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
+        // Update button text
+        setButtonText("Message Sent!");
+        
+        // Clear the form and reset button text after 2 seconds
+        setTimeout(() => {
+          form.current.reset(); // Clear form fields
+          setButtonText("Send"); // Reset button text
+        }, 2000);
+
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
   };
 
@@ -22,12 +32,12 @@ function Contact () {
     <form className='contact-form' ref={form} onSubmit={sendEmail} style={{display: 'flex', flexDirection: 'column'}}>
       <h1 className='contact-header' style={{color: '#fee7d5', paddingBottom: 60, textAlign: 'center'}}> Leave Me a Message 👇</h1>
       <label>Name</label>
-      <input className='contact-input' type="text" name="user_name" />
+      <input className='contact-input' style={{padding: 20}} type="text" name="user_name" />
       <label>Email</label>
-      <input className='contact-input' type="email" name="user_email" />
+      <input className='contact-input' style={{padding: 20}} type="email" name="user_email" />
       <label>Message</label>
-      <textarea className='contact-input' name="message" style={{width: '100%', height: '80px', maxWidth: '100%'}} />
-      <input className='contact-input-button' type="submit" value="Send" />
+      <textarea className='contact-input' name="message" style={{padding: 20, width: '100%', height: '80px', maxWidth: '100%'}} />
+      <input className='contact-input-button' type="submit" value={buttonText} />
     </form>
     </div>
   )
