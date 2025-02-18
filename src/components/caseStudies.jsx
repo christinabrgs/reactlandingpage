fcc9465e1d127ac9f00a9dc6d7faf062573548f4
christina.brgs@gmail.com <christina.brgs@gmail.com>
2025-02-18 14:56:39 -0500

    path fixes

--- src/components/caseStudies.jsx
+++ src/components/caseStudies.jsx
@@ -1,57 +1,58 @@
 import React from 'react'
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick'
 import Coached from '../images/coached.svg'
 import Explore from '../images/explore.svg'
 import "../components/aboutMe.css";
 import useIntersectionObserver from '../hooks/useIsVisible';
+import { Link } from 'react-router-dom';
 
-function CaseStudies () {
+function CaseStudies() {
   const { isVisible, domRef } = useIntersectionObserver();
 
 
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 2, // Number of slides to show
-        slidesToScroll: 1, // Number of slides to scroll
-        responsive: [
-          {
-            breakpoint: 780, // Screen width of 780px or less
-            settings: {
-              slidesToShow: 1, // Show only 1 slide
-            }
-          }
-        ]
-      };
-    
-      const slides = [
-        { id: 1, src: Explore, href: '/explore' },
-        { id: 2, src: Coached, href: '/coached' },
-        // { id: 3, src: CaseStudy },
-      ];
-    
-      return (
-        <div className={`fade-in-section ${isVisible ? "is-visible" : ""}`} ref={domRef}>
-        <div className='projects_slider' style={{height: '100%', paddingBottom: 100}}>
-          <div className="projects_headline"> <h1> CASE STUDIES </h1> </div>
-          <Slider style={{width: '70%', margin: 'auto'}} {...settings}>
-            {slides.map(slide => (
-              <div key={slide.id} style={{ height: '100%', width: '100%'}}>
-                         <a
-                href={slide.href}
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2, // Number of slides to show
+    slidesToScroll: 1, // Number of slides to scroll
+    responsive: [
+      {
+        breakpoint: 780, // Screen width of 780px or less
+        settings: {
+          slidesToShow: 1, // Show only 1 slide
+        }
+      }
+    ]
+  };
+
+  const slides = [
+    { id: 1, src: Explore, href: '/explore' },
+    { id: 2, src: Coached, href: '/coached' },
+    // { id: 3, src: CaseStudy },
+  ];
+
+  return (
+    <div className={`fade-in-section ${isVisible ? "is-visible" : ""}`} ref={domRef}>
+      <div className='projects_slider' style={{ height: '100%', paddingBottom: 100 }}>
+        <div className="projects_headline"> <h1> CASE STUDIES </h1> </div>
+        <Slider style={{ width: '70%', margin: 'auto' }} {...settings}>
+          {slides.map(slide => (
+            <div key={slide.id} style={{ height: '100%', width: '100%' }}>
+              <Link
+                to={slide.href}
                 rel="noreferrer"
               >
-                <img style={{height: '100%', width: '100%', padding: 2}} src={slide.src} alt='case-study' />
-                </a>
-              </div>
-            ))}
-          </Slider>
-        </div>
-        </div>
-      );
+                <img style={{ height: '100%', width: '100%', padding: 2 }} src={slide.src} alt='case-study' />
+              </Link>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
 }
 
 export default CaseStudies
